cd904b308c99b6db5f2db011bbae220d43ab046c
Saveli <oisurka@gmail.com>
2020-04-09 12:06:31 +0300

    feat: add 5th task

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,11 +1,20 @@
 package subtask5
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
 
     // TODO: Complete the following function
     fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        val arr = blockA.toMutableList().filter { it::class == blockB }
+        return when (blockB) {
+            Int::class -> arr.sumBy { it.toString().toInt() }
+            String::class -> arr.joinToString("")
+            LocalDate::class -> arr.stream().map { it as LocalDate }.max(LocalDate::compareTo).get()
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+            else -> ""
+        }
     }
 }
